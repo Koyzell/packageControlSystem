2dd787284bf3292488ab9b0075e48ea26a533227
Tianyu Jiang <15933666027@163.com>
2026-06-27 11:43:54 +0800

    无

---
+++ src/main/java/com/ustb/PackageControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ustb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageControlSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageControlSystemApplication.class, args);
+    }
+
+}
